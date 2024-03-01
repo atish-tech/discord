3c6e522f70d37ebc472b4460c0b46c66ad1706c9
Aatish <katish332@gmail.com>
2024-03-01 14:11:52 +0530

    group header , chat header

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,10 +1,48 @@
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ServerIdPage = () => {
-  return (
-    <div>
-        Server id
-    </div>
-  )
+interface ServerIdProps {
+  params : {
+    serverId : string
+  }
+};
+
+const ServerIdPage = async ({params} : ServerIdProps) => {
+  const profile = await currentProfile();
+  if(!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where : {
+      id : params.serverId,
+      members : {
+        some : {
+          profileId: profile.id,
+        }
+      }
+    },
+    include : {
+      chanels : {
+        where : {
+          name : "general"
+        },
+        orderBy : {
+          createdAt : "asc"
+        }
+      }
+    }
+  });
+
+  const initialChannel = server?.chanels[0];
+
+  if(initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
 
 export default ServerIdPage;
