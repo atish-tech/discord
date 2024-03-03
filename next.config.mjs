481230211223b9117918cdfab4ee9775a33adf27
Aatish <katish332@gmail.com>
2024-03-03 18:35:11 +0530

    fix libfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.externals.push({
+          "utf-8-validate": "commonjs utf-8-validate",
+          bufferutil: "commonjs bufferutil"
+        });
+    
+        return config;
+      },
+      typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
     images: {
         domains: ["uploadthing.com" , "utfs.io"]
     }
